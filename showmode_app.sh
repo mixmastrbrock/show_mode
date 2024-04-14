df18479ec7257c10469c432d74c402936aeca592
mixmastrbrock <66269339+mixmastrbrock@users.noreply.github.com>
2024-04-14 16:56:06 -0500

    Update showmode_app.sh

--- showmode_app.sh
+++ showmode_app.sh
@@ -291,12 +291,7 @@ pretty_print "Here we go..."
                 chmod +x "$SCRIPT_PATH"
             fi
             ###--- PLIST UPDATE ---###
-            SCRIPT_URL="https://raw.githubusercontent.com/mixmastrbrock/show_mode/main/showmode.plist"
-            SCRIPT_PATH="showmode.plist"
-            if curl --silent --head --fail "$SCRIPT_URL" > /dev/null; then
-                curl --silent --output "$SCRIPT_PATH" "$SCRIPT_URL"
-                chmod +x "$SCRIPT_PATH"
-            fi
+              wget -o -q https://raw.githubusercontent.com/mixmastrbrock/show_mode/main/showmode.plist
               mv -f showmode.plist ~/Library/LaunchAgents/
               launchctl load ~/Library/LaunchAgents/showmode.plist
               cron_command="0 0 1 * * ~/settings_cron.sh"
